b8d6b0b161660f3d3357215f95bd41aecb2123a6
Igor Kurganov <regkip@mail.ru>
2024-06-09 23:11:56 +0300

    Добавьте файлы проекта.

---
+++ Altex/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Altex.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
